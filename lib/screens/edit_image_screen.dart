1b606ce68da38123652bb70e38ba3d823f1d4a75
Junaidchohane <junaidchohanedu@gmail.com>
2026-01-26 13:00:43 +0500

    Adding New Text Logic

--- lib/screens/edit_image_screen.dart
+++ lib/screens/edit_image_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_editing_app/widgets/edit_image_viewmodel.dart';
+import 'package:image_editing_app/widgets/image_text.dart';
 
 class EditImageScreen extends StatefulWidget {
   const EditImageScreen({super.key, required this.selectImage});
@@ -15,10 +16,67 @@ class _EditImageScreenState extends EditImageViewmodel {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.file(File(widget.selectImage)),
+      body: SafeArea(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.3,
+          child: Stack(
+            children: [
+              _selectedImage,
+              for (int i = 0; i < texts.length; i++)
+                Positioned(
+                  left: texts[i].left,
+                  top: texts[i].top,
+                  child: GestureDetector(
+                    onLongPress: () {
+                      print("Long press detected");
+                    },
+                    onTap: () {
+                      print("Single press detected");
+                    },
+                    child: Draggable(
+                      feedback: ImageText(textInfo: texts[i]),
+                      child: ImageText(textInfo: texts[i]),
+                      onDragEnd: (drag) {
+                        final renderBox =
+                            context.findRenderObject() as RenderBox;
+                        Offset off = renderBox.globalToLocal(drag.offset);
+                        setState(() {
+                          texts[i].top = off.dy;
+                          texts[i].left = off.dx;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              creatorText.text.isNotEmpty
+                  ? Positioned(
+                      left: 0,
+                      bottom: 0,
+                      child: Text(
+                        creatorText.text,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                      ),
+                    )
+                  : const SizedBox.shrink(),
+            ],
+          ),
+        ),
+      ),
       floatingActionButton: _addNewTextFab,
     );
   }
+
+  Widget get _selectedImage => Center(
+    child: Image.file(
+      File(widget.selectImage),
+      fit: BoxFit.fill,
+      width: MediaQuery.of(context).size.width,
+    ),
+  );
 
   Widget get _addNewTextFab => FloatingActionButton(
     onPressed: () => addNewDialog(context),
--- lib/widgets/edit_image_viewmodel.dart
+++ lib/widgets/edit_image_viewmodel.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:image_editing_app/models/text_info.dart';
 import 'package:image_editing_app/screens/edit_image_screen.dart';
 import 'package:image_editing_app/widgets/default_button.dart';
 
 abstract class EditImageViewmodel extends State<EditImageScreen> {
   TextEditingController textEditingController = TextEditingController();
+  List<TextInfo> texts = [];
+
+  void addNewText(BuildContext content) {
+    setState(() {
+      texts.add(
+        TextInfo(
+          text: textEditingController.text,
+          left: 0,
+          top: 0,
+          color: Colors.black,
+          fontWeight: FontWeight.normal,
+          fontStyle: FontStyle.normal,
+          fontSize: 20,
+          textAlign: TextAlign.left,
+        ),
+      );
+      Navigator.of(context).pop();
+    });
+  }
+
   void addNewDialog(context) {
     showDialog(
       context: context,
