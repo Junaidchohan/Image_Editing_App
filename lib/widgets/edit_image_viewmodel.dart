510e5d10d9d608e210c48e3bcc1efbb5a6268aea
Junaidchohane <junaidchohanedu@gmail.com>
2026-01-27 18:42:58 +0500

    Saving Image to Gallery

--- lib/widgets/edit_image_viewmodel.dart
+++ lib/widgets/edit_image_viewmodel.dart
@@ -1,20 +1,54 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image_editing_app/models/text_info.dart';
 import 'package:image_editing_app/screens/edit_image_screen.dart';
+import 'package:image_editing_app/utils/utils.dart';
 import 'package:image_editing_app/widgets/default_button.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screenshot/screenshot.dart';
 
-abstract class EditImageViewmodel extends State<EditImageScreen> {
+abstract class EditImageViewModel extends State<EditImageScreen> {
   TextEditingController textEditingController = TextEditingController();
   TextEditingController creatorText = TextEditingController();
+  ScreenshotController screenshotController = ScreenshotController();
+
   List<TextInfo> texts = [];
   int currentIndex = 0;
 
-  void removeText() {
+  void saveToGallery(BuildContext context) {
+    if (texts.isNotEmpty) {
+      screenshotController
+          .capture()
+          .then((Uint8List? image) {
+            saveImage(image!);
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Image saved to gallery.')),
+            );
+          })
+          .catchError((err) => print(err));
+    }
+  }
+
+  saveImage(Uint8List bytes) async {
+    final time = DateTime.now()
+        .toIso8601String()
+        .replaceAll('.', '-')
+        .replaceAll(':', '-');
+    final name = "screenshot_$time";
+    await requestPermission(Permission.storage);
+    await ImageGallerySaver.saveImage(bytes, name: name);
+  }
+
+  void removeText(BuildContext context) {
     setState(() {
       texts.removeAt(currentIndex);
     });
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("Deleted", style: TextStyle(fontSize: 16.0))),
+      const SnackBar(
+        content: Text('Deleted', style: TextStyle(fontSize: 16.0)),
+      ),
     );
   }
 
@@ -23,8 +57,8 @@ abstract class EditImageViewmodel extends State<EditImageScreen> {
       currentIndex = index;
     });
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text("Selected for Styling", style: TextStyle(fontSize: 16.0)),
+      const SnackBar(
+        content: Text('Selected For Styling', style: TextStyle(fontSize: 16.0)),
       ),
     );
   }
@@ -37,13 +71,13 @@ abstract class EditImageViewmodel extends State<EditImageScreen> {
 
   void increaseFontSize() {
     setState(() {
-      texts[currentIndex].fontSize = texts[currentIndex].fontSize += 2;
+      texts[currentIndex].fontSize += 2;
     });
   }
 
   void decreaseFontSize() {
     setState(() {
-      texts[currentIndex].fontSize = texts[currentIndex].fontSize -= 2;
+      texts[currentIndex].fontSize -= 2;
     });
   }
 
@@ -68,9 +102,9 @@ abstract class EditImageViewmodel extends State<EditImageScreen> {
   void boldText() {
     setState(() {
       if (texts[currentIndex].fontWeight == FontWeight.bold) {
-        texts[currentIndex].fontWeight == FontWeight.normal;
+        texts[currentIndex].fontWeight = FontWeight.normal;
       } else {
-        texts[currentIndex].fontWeight == FontWeight.bold;
+        texts[currentIndex].fontWeight = FontWeight.bold;
       }
     });
   }
@@ -78,26 +112,30 @@ abstract class EditImageViewmodel extends State<EditImageScreen> {
   void italicText() {
     setState(() {
       if (texts[currentIndex].fontStyle == FontStyle.italic) {
-        texts[currentIndex].fontStyle == FontStyle.normal;
+        texts[currentIndex].fontStyle = FontStyle.normal;
       } else {
-        texts[currentIndex].fontStyle == FontStyle.italic;
+        texts[currentIndex].fontStyle = FontStyle.italic;
       }
     });
   }
 
   void addLinesToText() {
     setState(() {
-      if (texts[currentIndex].text.contains("\n")) {
-        texts[currentIndex].text ==
-            texts[currentIndex].text.replaceAll("\n", " ");
+      if (texts[currentIndex].text.contains('\n')) {
+        texts[currentIndex].text = texts[currentIndex].text.replaceAll(
+          '\n',
+          ' ',
+        );
       } else {
-        texts[currentIndex].text ==
-            texts[currentIndex].text.replaceAll(" ", "\n");
+        texts[currentIndex].text = texts[currentIndex].text.replaceAll(
+          ' ',
+          '\n',
+        );
       }
     });
   }
 
-  void addNewText(BuildContext content) {
+  void addNewText(BuildContext context) {
     setState(() {
       texts.add(
         TextInfo(
@@ -115,18 +153,18 @@ abstract class EditImageViewmodel extends State<EditImageScreen> {
     });
   }
 
-  void addNewDialog(context) {
+  addNewDialog(context) {
     showDialog(
       context: context,
       builder: (BuildContext context) => AlertDialog(
-        title: Text("Add New Text"),
+        title: const Text('Add New Text'),
         content: TextField(
           controller: textEditingController,
           maxLines: 5,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             suffixIcon: Icon(Icons.edit),
             filled: true,
-            hintText: "Your Text Here...",
+            hintText: 'Your Text Here..',
           ),
         ),
         actions: <Widget>[
@@ -134,13 +172,13 @@ abstract class EditImageViewmodel extends State<EditImageScreen> {
             onPressed: () => Navigator.of(context).pop(),
             color: Colors.white,
             textColor: Colors.black,
-            child: Text("Back"),
+            child: const Text('Back'),
           ),
           DefaultButton(
             onPressed: () => addNewText(context),
             color: Colors.red,
             textColor: Colors.white,
-            child: Text("Add Text"),
+            child: const Text('Add Text'),
           ),
         ],
       ),
